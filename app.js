025ee299144616b0aa7075042893d5c97b5b4634
alsrudursla <nuy0307@naver.com>
2023-05-01 16:22:44 +0900

    login

--- app.js
+++ app.js
@@ -2,15 +2,49 @@
 // const loginInput = loginForm.querySelector("input");
 // const loginButton = loginForm.querySelector("button");
 
+
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form")
 //const loginButton = document.querySelector("#login-form button");
 
-function onLoginSubmit(event) {
-    //const username = loginInput.value;
-    //console.log(username);
-    event.preventDefault();
-    console.log(loginInput.value)
+
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+const USERNAME_KEY = "username";
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
+
+function onLoginSubmit(event) {
+    event.preventDefault();     // 브라우저의 기본 동작 막기
+    loginForm.classList.add(HIDDEN_CLASSNAME); // form 에 클래스 추가 (hidden 클래스 추가 - css 에 정의되어 있음)
+    const username = loginInput.value;
+
+    localStorage.setItem(USERNAME_KEY, username);
+
+    // greeting.innerText = "Hello " + username;
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
 //loginButton.addEventListener("click", onLoginBtnClick);
 loginForm.addEventListener("submit", onLoginSubmit);
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    // show the greeting
+    paintGreetings(savedUsername);
+}
